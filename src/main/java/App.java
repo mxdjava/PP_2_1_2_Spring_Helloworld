f40d9f44bad36261cc702442decafdb0bf1e08c7
mxdjava <charismaticdrugger@gmail.com>
2023-06-22 21:46:16 +0600

    Работает

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,24 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        Cat bean2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean2.getVoice());
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getVoice());
+        if (bean == bean1) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+        if (bean2 == bean3) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
     }
 }
